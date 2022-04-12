33ab6a9f8c4590134a98a50d92b11cc36e062ae3
KEERTHANA-S-N <95872941+KEERTHANA-S-N@users.noreply.github.com>
2022-04-12 12:46:55 +0530

    Add files via upload

---
+++ Studentdb.sql
@@ -0,0 +1,51 @@
+show databases;
+use mysql;
+show tables;
+select * from component;
+create table students
+(username varchar(201),
+email varchar(201),
+passwrd varchar(202));
+select * from students;
+insert into students 
+values('keerthana', 'keerthana.sn8@gmail.com','ga@69');
+select * from students;
+insert into students 
+values('sumi', 'sumithranagraj15@gmail.com','abc12');
+select * from students;
+insert into students
+values('Nagaraj', 'nagarajdsagar156@gmail.com','nagraj16');
+select * from students;
+insert into students 
+values('lakshmi', 'lakshmi2@gmail.com','lakshmi@');
+select * from students;
+insert into students
+values('Arnav', 'Arnav2@gmail.com','abc12');
+select * from students;
+insert into students 
+values('lokesh', 'lokesh2@gmail.com','lokesh@');
+select * from students;
+insert into students
+values('amith', 'Amith12@gmail.com','abc1256');
+select * from students;
+insert into students 
+values('bindhu', 'bindhu2@gmail.com','biii@');
+select * from students;
+insert into students
+values('suman', 'Suman@gmail.com','bde2');
+select * from students;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
